22e400415ac437fc8d9507b29377579ef61760b2
femiooboy <okonoluwaferanmi@gmail.com>
2024-11-22 11:14:11 +0100

    added a new origin

--- server/index.ts
+++ server/index.ts
@@ -34,13 +34,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-      } else {
-          callback(new Error("Not allowed by CORS"));
-      }
-  },
+  origin:"https://finance-care-1.vercel.app",
   credentials: true,
   
 }));
